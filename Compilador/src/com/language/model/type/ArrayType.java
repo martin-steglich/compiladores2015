d81c2249af9c2c4245601aa96c6d67cf1cc5d68a
cotyrodri <cotyrodri@hotmail.com>
2015-07-13 21:44:05 -0300

    ArrayType

--- Compilador/src/com/language/model/type/ArrayType.java
+++ Compilador/src/com/language/model/type/ArrayType.java
@@ -3,26 +3,25 @@ package com.language.model.type;
 import com.language.model.type.Type;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import com.language.model.expression.Expression;
 
 public class ArrayType extends Type {
-	private List<Expression> list;
+	
+	private ArrayList<Type> list;
 	
 	public ArrayType() {
-		list = new ArrayList<Expression>();
+		list = new ArrayList<Type>();
 		
 	}
 
-	public ArrayType(List<Expression> list){
+	public ArrayType(ArrayList<Type> list){
         this.list = list;
     }
 
-    public List<Expression> getList(){
+    public ArrayList<Type> getList(){
         return list;
     }
-    public void setList(List<Expression> l){
+    public void setList(ArrayList<Type> l){
         list=l;
     }
 
@@ -36,11 +35,25 @@ public class ArrayType extends Type {
     
 	@Override
 	public TypeEnum getTypeEnum() {
-		//
+		return TypeEnum.LIST;
 	}
 	
 	@Override
 	public String getAsString() {
-		//
+		String l = "[";
+		int length = list.size();
+		for (Type type : list){
+			l += type.getAsString();
+			if(length > 1) {
+				l += ",";
+			}
+			length--;
+		}
+		l += ")";
+		return l;
+	}
+	
+	public boolean getBooleanValue(){
+		return false;
 	}
 }
