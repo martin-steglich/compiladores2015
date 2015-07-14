fefffa6706e20b495d51a166639f25ad825121f1
cotyrodri <cotyrodri@hotmail.com>
2015-07-14 19:03:53 -0300

    Funcionan todos los casos de lista y tupla

--- Compilador/StructuredTypesTest.py
+++ Compilador/StructuredTypesTest.py
@@ -1,11 +1,7 @@
-<<<<<<< Updated upstream
-a = (3,1,2,0,4,5)
-b = [1,2,3,4]
-print b[0:4]
-=======
 a = [0,18,3,6,8,10,12,54]
 b = (0,18,3,6,8,10,12,54)
 print "lista"
+print a[-3]
 print a[3]
 print a[0:2]
 print a[0:3:2]
@@ -16,6 +12,7 @@ print a[::2]
 print a[:4:1]
 print a[4::3]
 print "tupla"
+print b[-3]
 print b[3]
 print b[0:2]
 print b[0:3:2]
@@ -25,4 +22,3 @@ print b[:]
 print b[::2]
 print b[:4:1]
 print b[4::3]
->>>>>>> Stashed changes
