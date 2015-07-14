e20b5f7f5868defc02993bf6a98a592d3dea08a3
cotyrodri <cotyrodri@hotmail.com>
2015-07-14 13:00:22 -0300

    Funcionan todos los casos menos [n:], habria que crear otra forma para [n]

--- Compilador/src/com/language/model/expression/StructuredTypePositionExpression.java
+++ Compilador/src/com/language/model/expression/StructuredTypePositionExpression.java
@@ -9,6 +9,7 @@ import com.language.model.type.DictionaryType;
 import com.language.model.type.IntegerType;
 import com.language.model.type.LongType;
 import com.language.model.type.TupleType;
+import com.language.model.type.ArrayType;
 import com.language.model.type.Type;
 
 public class StructuredTypePositionExpression extends Expression{
@@ -16,14 +17,16 @@ public class StructuredTypePositionExpression extends Expression{
 	IdentifierExpression id;
 	Expression startPosition;
 	Expression endPosition;
+	Expression cantPos;
 	int lineNumber;
 	
 	
 	
-	public StructuredTypePositionExpression(IdentifierExpression id, Expression startPosition, Expression endPosition) {
+	public StructuredTypePositionExpression(IdentifierExpression id, Expression startPosition, Expression endPosition, Expression cantPos) {
 		this.id = id;
 		this.startPosition = startPosition;
 		this.endPosition = endPosition; 
+		this.cantPos = cantPos; 
 //		this.lineNumber = lineNumber;
 	}
 
@@ -34,66 +37,246 @@ public class StructuredTypePositionExpression extends Expression{
 	@Override
 	public Type evaluate() throws CompilerException {
 		Type structuredType = id.evaluate();
-		Type startPos = startPosition.evaluate();
 		if(structuredType.getType() == 10){ //DICT
+			Type startPos = startPosition.evaluate();
 			Map<Type, Type> dictionary = ((DictionaryType)structuredType).getDictionary();
 			if(dictionary.containsKey(startPos)){
 				return dictionary.get(startPos);
 			} else
 				throw new CompilerException(lineNumber, "La variable '" + id.getId() + "' no contiene la entrada '" + startPos.getAsString() + "'");
 		} else if(structuredType.getType() == 6){ //LIST
+			if (startPosition!= null){
+				Type startPos = startPosition.evaluate();
+				if((startPos.getType() != 2) && (startPos.getType() != 0) && (startPos.getType() != 5))
+					throw new CompilerException(lineNumber, "El indice de las listas debe ser entero");
+				if(startPos.getType() == 5){
+					long start = (long)((BooleanType)startPos).getEquivalentInt();
+					startPos = new LongType(start);
+				}
+				if(startPos.getType() == 0){
+					long start = (long)((IntegerType)startPos).getValue();
+					startPos = new LongType(start);
+				}
+				ArrayList<Type> list = ((ArrayType)structuredType).getList();
+				int size = list.size();
+				
+				if(((LongType)startPos).getValue() >= size)
+					throw new CompilerException(lineNumber, "El indice de la lista esta fuera de rango");
+				if ((cantPos == null) && (endPosition == null)){
+					int index = (int)((LongType)startPos).getValue();
+					Type ret = list.get(index);
+					return ret;
+				}else{
+					if (endPosition != null){
+						Type endPos = endPosition.evaluate();
+						if((endPos.getType() != 2) && (endPos.getType() != 0) && (endPos.getType() != 5))
+							throw new CompilerException(lineNumber, "El indice de las listas debe ser entero");
+						if(endPos.getType() == 5){
+							long end = (long)((BooleanType)endPos).getEquivalentInt();
+							endPos = new LongType(end);
+						}
+						if(endPos.getType() == 0){
+							long end = (long)((IntegerType)endPos).getValue();
+							endPos = new LongType(end);
+						}
+						
+						if(((LongType)endPos).getValue() > size)
+							throw new CompilerException(lineNumber, "El indice de la lista esta fuera de rango");
+						ArrayList<Type> ListaRet = new ArrayList<>();
+						if (cantPos != null){
+							int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+							for(long i = ((LongType)startPos).getValue(); i < ((LongType)endPos).getValue(); i= i + (long)valPos){
+								ListaRet.add(list.get((int)i));
+							}
+						}else{
+							for(long i = ((LongType)startPos).getValue(); i < ((LongType)endPos).getValue(); i++){
+								ListaRet.add(list.get((int)i));
+							}
+						}
+						return new ArrayType(ListaRet);
+										
+					}else{
+						ArrayList<Type> ListaRet = new ArrayList<>();
+						if (cantPos != null){
+							int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+							for(long i = ((LongType)startPos).getValue(); i < size; i= i + (long)valPos){
+								ListaRet.add(list.get((int)i));
+							}
+						}else{
+							for(int i = ((IntegerType)startPos).getValue(); i < (int)size; i++){
+								ListaRet.add(list.get(i));
+							}
+						}
+						return new ArrayType(ListaRet);
+					}
+					
+				}
 			
+			}else{
+				
+				ArrayList<Type> list = ((ArrayType)structuredType).getList();
+				int size = list.size();
+				ArrayList<Type> ListRet = new ArrayList<>();
+				if (endPosition != null){
+					Type endPos = endPosition.evaluate();
+					if((endPos.getType() != 2) && (endPos.getType() != 0) && (endPos.getType() != 5))
+						throw new CompilerException(lineNumber, "El indice de las listas debe ser entero");
+					if(endPos.getType() == 5){
+						long end = (long)((BooleanType)endPos).getEquivalentInt();
+						endPos = new LongType(end);
+					}
+					if(endPos.getType() == 0){
+						long end = (long)((IntegerType)endPos).getValue();
+						endPos = new LongType(end);
+					}
+					
+					if(((LongType)endPos).getValue() > size)
+						throw new CompilerException(lineNumber, "El indice de la tupla esta fuera de rango");		
+					if (cantPos != null){
+						int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+						for(int i = 0; i < ((LongType)endPos).getValue(); i= i + valPos){
+							ListRet.add(list.get((int)i));
+						}
+					}else{
+						for(int i = 0; i < ((LongType)endPos).getValue(); i++){
+							ListRet.add(list.get((int)i));
+						}
+					}
+				}else{
+					if (cantPos != null){
+						int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+						for(int i = 0; i < size; i= i + valPos){
+							ListRet.add(list.get((int)i));
+						}
+					}else{
+						for(int i = 0; i < size; i++){
+							ListRet.add(list.get((int)i));
+						}
+					}
+					
+				}
+				return new ArrayType(ListRet);
+			}	
 		} else if(structuredType.getType() == 11){ //TUPLE
-			
-			if((startPos.getType() != 2) && (startPos.getType() != 0) && (startPos.getType() != 5))
-				throw new CompilerException(lineNumber, "El indice de las tuplas debe ser entero");
-			if(startPos.getType() == 5){
-				long start = (long)((BooleanType)startPos).getEquivalentInt();
-				startPos = new LongType(start);
-			}
-			if(startPos.getType() == 0){
-				long start = (long)((IntegerType)startPos).getValue();
-				startPos = new LongType(start);
-			}
-			ArrayList<Type> tuple = ((TupleType)structuredType).getTuple();
-			int size = tuple.size();
-			
-			if(((LongType)startPos).getValue() >= size)
-				throw new CompilerException(lineNumber, "El indice de la tupla esta fuera de rango");
-			
-			if (endPosition != null){
-				Type endPos = endPosition.evaluate();
-				if((endPos.getType() != 2) && (endPos.getType() != 0) && (endPos.getType() != 5))
+			if (startPosition!= null){
+				Type startPos = startPosition.evaluate();
+				if((startPos.getType() != 2) && (startPos.getType() != 0) && (startPos.getType() != 5))
 					throw new CompilerException(lineNumber, "El indice de las tuplas debe ser entero");
-				if(endPos.getType() == 5){
-					long end = (long)((BooleanType)endPos).getEquivalentInt();
-					endPos = new LongType(end);
+				if(startPos.getType() == 5){
+					long start = (long)((BooleanType)startPos).getEquivalentInt();
+					startPos = new LongType(start);
 				}
-				if(endPos.getType() == 0){
-					long end = (long)((IntegerType)endPos).getValue();
-					endPos = new LongType(end);
+				if(startPos.getType() == 0){
+					long start = (long)((IntegerType)startPos).getValue();
+					startPos = new LongType(start);
 				}
+				ArrayList<Type> tuple = ((TupleType)structuredType).getTuple();
+				int size = tuple.size();
 				
-				if(((LongType)endPos).getValue() >= size)
+				if(((LongType)startPos).getValue() >= size)
 					throw new CompilerException(lineNumber, "El indice de la tupla esta fuera de rango");
-				
-				ArrayList<Type> tupleRet = new ArrayList<>();
-				for(long i = ((LongType)startPos).getValue(); i < ((LongType)endPos).getValue(); i++){
-					tupleRet.add(tuple.get((int)i));
+				if ((cantPos == null) && (endPosition == null)){
+					int index = (int)((LongType)startPos).getValue();
+					Type ret = tuple.get(index);
+					return ret;
+				}else{
+					if (endPosition != null){
+						Type endPos = endPosition.evaluate();
+						if((endPos.getType() != 2) && (endPos.getType() != 0) && (endPos.getType() != 5))
+							throw new CompilerException(lineNumber, "El indice de las tuplas debe ser entero");
+						if(endPos.getType() == 5){
+							long end = (long)((BooleanType)endPos).getEquivalentInt();
+							endPos = new LongType(end);
+						}
+						if(endPos.getType() == 0){
+							long end = (long)((IntegerType)endPos).getValue();
+							endPos = new LongType(end);
+						}
+						
+						if(((LongType)endPos).getValue() > size)
+							throw new CompilerException(lineNumber, "El indice de la tupla esta fuera de rango");
+						ArrayList<Type> tupleRet = new ArrayList<>();
+						if (cantPos != null){
+							int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+							for(long i = ((LongType)startPos).getValue(); i < ((LongType)endPos).getValue(); i= i + (long)valPos){
+								tupleRet.add(tuple.get((int)i));
+							}
+						}else{
+							for(long i = ((LongType)startPos).getValue(); i < ((LongType)endPos).getValue(); i++){
+								tupleRet.add(tuple.get((int)i));
+							}
+						}
+						return new TupleType(tupleRet);
+										
+					}else{
+						ArrayList<Type> tupleRet = new ArrayList<>();
+						if (cantPos != null){
+							int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+							for(long i = ((LongType)startPos).getValue(); i < size; i= i + (long)valPos){
+								tupleRet.add(tuple.get((int)i));
+							}
+						}else{
+							for(int i = ((IntegerType)startPos).getValue(); i < (int)size; i++){
+								tupleRet.add(tuple.get(i));
+							}
+						}
+						return new TupleType(tupleRet);
+					}
+					
 				}
-				
-				return new TupleType(tupleRet);
-								
-			}
 			
-			int index = (int)((LongType)startPos).getValue();
-			Type ret = tuple.get(index);
-			return ret;
+			}else{
+				ArrayList<Type> tuple = ((TupleType)structuredType).getTuple();
+				int size = tuple.size();
+				ArrayList<Type> tupleRet = new ArrayList<>();
+				if (endPosition != null){
+					Type endPos = endPosition.evaluate();
+					if((endPos.getType() != 2) && (endPos.getType() != 0) && (endPos.getType() != 5))
+						throw new CompilerException(lineNumber, "El indice de las tuplas debe ser entero");
+					if(endPos.getType() == 5){
+						long end = (long)((BooleanType)endPos).getEquivalentInt();
+						endPos = new LongType(end);
+					}
+					if(endPos.getType() == 0){
+						long end = (long)((IntegerType)endPos).getValue();
+						endPos = new LongType(end);
+					}
+					
+					if(((LongType)endPos).getValue() > size)
+						throw new CompilerException(lineNumber, "El indice de la tupla esta fuera de rango");		
+					if (cantPos != null){
+						int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+						for(int i = 0; i < ((LongType)endPos).getValue(); i= i + valPos){
+							tupleRet.add(tuple.get((int)i));
+						}
+					}else{
+						for(int i = 0; i < ((LongType)endPos).getValue(); i++){
+							tupleRet.add(tuple.get((int)i));
+						}
+					}
+				}else{
+					if (cantPos != null){
+						int valPos = ((IntegerType)(cantPos.evaluate())).getValue();
+						for(int i = 0; i < size; i= i + valPos){
+							tupleRet.add(tuple.get((int)i));
+						}
+					}else{
+						for(int i = 0; i < size; i++){
+							tupleRet.add(tuple.get((int)i));
+						}
+					}
+					
+				}
+				return new TupleType(tupleRet);
+			}	
 		} else
 			throw new CompilerException(lineNumber, "La variable no es un Tipo de Datos Estructurado (Diccionario, Lista, Tupla)");
 			
-		return startPos;
+			
+		
 	}
+	
+
 
 	@Override
 	public String getType() {
